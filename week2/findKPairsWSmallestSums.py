1c61e88a8348c30e6d88d5b1b671310ef630bfc8
Joshua Chan <joshualchan@gmail.com>
2020-06-17 21:03:40 -0700

    Create findKPairsWSmallestSums.py
    done w/o a heap.

---
+++ week2/findKPairsWSmallestSums.py
@@ -0,0 +1,8 @@
+class Solution:
+    def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
+        answer = []
+        for i in nums1:
+            for j in nums2:
+                answer.append([i,j])
+        answer = sorted(answer, key = lambda t: sum(t))
+        return answer[:k]
